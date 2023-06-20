29f189d485869aeb8955eb46ed1beb725d05c2da
tinleviet <10422078@student.vgu.edu.vn>
2023-06-20 15:07:27 +0700

    message

--- detect.py
+++ detect.py
@@ -1,6 +1,7 @@
-from keras.models import load_model  # TensorFlow is required for Keras to work
-from PIL import Image, ImageOps  # Install pillow instead of PIL
+from keras.models import load_model
+from PIL import Image, ImageOps
 import numpy as np
+import glob
 
 # Disable scientific notation for clarity
 np.set_printoptions(suppress=True)
@@ -9,34 +10,50 @@ np.set_printoptions(suppress=True)
 model = load_model("converted_keras/keras_model.h5", compile=False)
 
 # Load the labels
-class_names = open("converted_keras/labels.txt", "r", encoding="utf-8").read
-# Create the array of the right shape to feed into the keras model
-# The 'length' or number of images you can put into the array is
-# determined by the first position in the shape tuple, in this case 1
-data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+class_names = open("converted_keras/labels.txt", "r", encoding="utf-8").read().splitlines()
 
-# Replace this with the path to your image
-image = Image.open("WIN_20230607_10_33_22_Pro.jpg").convert("RGB")
+# Create an empty list to store the predictions
+predictions = []
 
-# resizing the image to be at least 224x224 and then cropping from the center
-size = (224, 224)
-image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
+# Replace this with the path to your image folder
+image_folder = "data"
 
-# turn the image into a numpy array
-image_array = np.asarray(image)
+# Retrieve a list of image paths from the folder
+image_paths = glob.glob(image_folder + "/*.jpg")
 
-# Normalize the image
-normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
+# Iterate over the image paths
+for image_path in image_paths:
+    # Create the array of the right shape to feed into the keras model
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
-# Load the image into the array
-data[0] = normalized_image_array
+    # Load the image
+    image = Image.open(image_path).convert("RGB")
 
-# Predicts the model
-prediction = model.predict(data)
-index = np.argmax(prediction)
-class_name = class_names[index]
-confidence_score = prediction[0][index]
+    # Resize and crop the image
+    size = (224, 224)
+    image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
 
-# Print prediction and confidence score
-print("Class:", class_name[2:], end="")
-print("Confidence Score:", confidence_score)
+    # Convert the image to a numpy array
+    image_array = np.asarray(image)
+
+    # Normalize the image
+    normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
+
+    # Load the image into the array
+    data[0] = normalized_image_array
+
+    # Make a prediction
+    prediction = model.predict(data)
+    index = np.argmax(prediction)
+    class_name = class_names[index]
+    confidence_score = prediction[0][index]
+
+    # Store the prediction
+    predictions.append((class_name, confidence_score))
+
+# Print the predictions
+for i, (class_name, confidence_score) in enumerate(predictions):
+    print(f"Image {i+1}:")
+    print("Class:", class_name)
+    print("Confidence Score:", confidence_score)
+    print()
